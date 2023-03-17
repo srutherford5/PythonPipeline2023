962096a68e07a5a760d028862961300a7e7ec275
Samantha Rutherford <sruthrford@luc.edu>
2023-03-17 04:56:52 -0500

    Pulled Genbank file and created a FASTA file with only coding sequences. Inital Kallisto index created. README updated with number of CDS.

--- PPcode.py
+++ PPcode.py
@@ -2,7 +2,8 @@
 import wget
 import sys
 import os
-
+from Bio import Entrez
+from Bio import SeqIO
 #Import test data by setting a stock URL variable
 url = 'https://sra-pub-run-odp.s3.amazonaws.com/sra/'
 #List of ids in order of patient and day 2 or 6 post infection
@@ -33,9 +34,7 @@ def out_folder(folder, ids):
 #Call output folder function with designated name and our ids list
 out_folder('PipelineProject_Samantha_Rutherford', sra_ids)
 
-
-
-#Assign each file name to a variable for now //break//
+#Assign each file name to a variable just in case
 d1_2_1 = sra_ids[0] + '_1.fastq'
 d1_2_2 = sra_ids[0] + '_2.fastq'
 d1_6_1 = sra_ids[1] + '_1.fastq'
@@ -44,3 +43,31 @@ d3_2_1 = sra_ids[2] + '_1.fastq'
 d3_2_2 = sra_ids[2] + '_2.fastq'
 d3_6_1 = sra_ids[3] + '_1.fastq'
 d3_6_2 = sra_ids[3] + '_2.fastq'
+
+
+#Begin part 2
+
+
+#Use Entrez to access the HCMV genome and SeqIO for later parsing
+Entrez.email = 'srutherford@luc.edu'
+handle = Entrez.efetch(db ='nucleotide', id = 'NC_006273.2', rettype = 'gbwithparts', retmode = 'text')
+genome = next(SeqIO.parse(handle, 'genbank'))
+#Create an empty list for coding sequences
+cds = []
+#Use for loop to access all CDS
+for feature in genome.features:
+    if feature.type == 'CDS':
+        cds.append(feature)
+#Second loop is created to remove genbank information so the sequence can be put into FASTA format
+seqs = []
+for feature in cds:
+    seq = genome[int(feature.location.start):int(feature.location.end)]
+    if feature.location.strand == -1:
+        seq.seq = seq.seq.reverse_complement()
+    seq.description = feature.qualifiers['product'][0]
+    seqs.append(seq)
+#Write coding sequences to a fasta file
+SeqIO.write(seqs, 'NC_006273.2.fasta', 'fasta')
+
+#Build the HCMV index for Kallisto with coding sequence fasta
+os.system('time kallisto index -i NC_006273.2.idx NC_006273.2.fasta --make-unique -k 31')
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # PythonPipeline2023
 The first function in the code is used to create the ouput folder and also call and split all donor reads
 I'm doing the differential expression track.
+The HCMV genome (NC_006273.2) has 169 CDS.
